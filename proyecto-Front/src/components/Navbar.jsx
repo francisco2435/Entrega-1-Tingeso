b1be99f75a172e0ec121f984a55d9641b3e0fc86
francisco2435 <francisco.riquelme.n@usach.cl>
2025-04-27 22:21:37 -0400

    Update Navbar.jsx

--- proyecto-Front/src/components/Navbar.jsx
+++ proyecto-Front/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography"; // Asegúrate de importar Typography
+import Typography from "@mui/material/Typography"; 
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -55,7 +55,7 @@ export default function Navbar() {
           </IconButton>
 
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Sistema de Karting
+            Sistema de Karting RM
           </Typography>
 
           {!usuario ? (
